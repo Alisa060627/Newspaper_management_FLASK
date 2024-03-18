9ceec1f6447f52b68d476ff800ad398f92df069d
Alisa Beztsinna <23imc10600@fh-krems.ac.at>
2024-03-18 17:56:57 +0100

    Requirements added

---
+++ src/model/issue.py
@@ -0,0 +1,9 @@
+class Issue(object):
+
+    def __init__(self, releasedate, released: bool = False):
+        self.releasedate = releasedate
+        self.released: bool = released
+
+    def set_editor(self, editor):
+        # TODO: Implement me!
+        pass
